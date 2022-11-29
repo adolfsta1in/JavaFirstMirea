0eff1dd335a088b08ba8ffd9665903a0d20972b6
Адис Ширинбек <adees.shirinbek@gmail.com>
2022-11-29 19:21:06 +0300

    Сделал по второй практике первое задание не до конца

---
+++ src/ru/mirea/task2/TestAuthor.java
@@ -0,0 +1,13 @@
+package ru.mirea.task2;
+import java.lang.*;
+import java.util.Scanner;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        String name;
+        String email;
+        char gender;
+        Author qwerty = new Author("Oleg", "oleg@mail.ru", 'M');
+        Author.toString();
+    }
+}
